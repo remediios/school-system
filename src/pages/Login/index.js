e9d0bbb006b8fb56a581413f851ce3275c15ed82
Miguel Remédios <miguelremediios@gmail.com>
2024-01-13 21:08:49 +0000

    axios global config

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Title } from './styled';
 import { Container } from '../../styles/GlobalStyles';
+import axios from '../../services/axios';
 
 function Login() {
+  React.useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get('/students');
+        const { data } = response;
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
+
   return (
     <Container>
       <Title isRed={false}>
