ff85242c9b5c58db37dac5bb01ea224f2636c4fd
Miguel Remédios <miguelremediios@gmail.com>
2024-01-23 22:44:16 +0000

    Deleting student functionality

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { get } from 'lodash';
 import { Link } from 'react-router-dom';
-import { FaUserCircle, FaEdit, FaTrash } from 'react-icons/fa';
+import { FaUserCircle, FaEdit, FaTrash, FaExclamation } from 'react-icons/fa';
 
+import { toast } from 'react-toastify';
 import axios from '../../services/axios';
 import { Container } from '../../styles/GlobalStyles';
 import { ProfilePicture, StudentContainer, StudentRow } from './styled';
@@ -24,13 +25,40 @@ function Students() {
     getData();
   }, []);
 
+  const handleDeleteAsk = (e) => {
+    e.preventDefault();
+    const exclamationIcon = e.currentTarget.nextSibling;
+    exclamationIcon.setAttribute('display', 'block');
+    e.currentTarget.remove();
+  };
+
+  const handleDeleteConfirm = async (e, id, index) => {
+    e.persist();
+    try {
+      setIsLoading(true);
+      await axios.delete(`/students/${id}`);
+      const newStudents = [...students];
+      newStudents.splice(index, 1);
+      setStudents(newStudents);
+      setIsLoading(false);
+    } catch (error) {
+      const status = get(error, 'response.status', 0);
+      if (status === 401) {
+        toast.error('Please sign-in before continuing...');
+      } else {
+        toast.error('Something went wrong...');
+      }
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <h1>Students</h1>
       <StudentContainer>
         {students &&
-          students.map((student) => (
+          students.map((student, index) => (
             <StudentRow key={String(student.id)}>
               <ProfilePicture>
                 {get(student, 'Photos[0].url', false) ? (
@@ -46,9 +74,19 @@ function Students() {
                 <FaEdit size={16} />
               </Link>
 
-              <Link to={`/student/${student.id}/delete`}>
+              <Link
+                onClick={handleDeleteAsk}
+                to={`/student/${student.id}/delete`}
+              >
                 <FaTrash size={16} />
               </Link>
+
+              <FaExclamation
+                onClick={(e) => handleDeleteConfirm(e, student.id, index)}
+                size={16}
+                display="none"
+                cursor="pointer"
+              />
             </StudentRow>
           ))}
       </StudentContainer>
